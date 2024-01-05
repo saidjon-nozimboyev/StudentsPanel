5a4b5f597ccf41484ca75cc15e0e2a1d96000fc3
user <user@WIN-9TE5F9EN5LS>
2024-01-05 21:33:22 +0500

    Add project files.

---
+++ StudentPanel/Class1.cs
@@ -0,0 +1,74 @@
+﻿namespace StudentPanel
+{
+    internal class cntId
+    {
+        public int id = 0;
+
+        public void plusID()
+        {
+            this.id++;
+        }
+    }
+
+    internal class Student
+    {
+        public string name;
+        public string surname;
+        public byte age;
+        public string number;
+        public string password;
+
+        public Student(string name, string surname, byte age, string number, string password)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.number = number;
+            this.password = password;
+        }
+    }
+
+    internal class recordStudent
+    {
+        private static cntId cntId = new cntId();
+        private static List<Student> studentList = new List<Student>();
+
+        public static void AddStudent(string name, string surname, byte age, string number, string password)
+        {
+            cntId.plusID();
+            int num = cntId.id;
+
+            Student newStudent = new Student(name, surname, age, number, password);
+            studentList.Add(newStudent);
+
+            Console.WriteLine($"Student {num} added successfully.");
+        }
+
+        public static void DisplayStudents()
+        {
+            Console.WriteLine("List of Students:");
+            Console.WriteLine("| {0,-15} | {1,-15} | {2,-5} | {3,-15} |", "Name", "Surname", "Age", "Number");
+
+            foreach (var student in studentList)
+            {
+                Console.WriteLine("| {0,-15} | {1,-15} | {2,-5} | {3,-15} |", student.name, student.surname, student.age, student.number);
+            }
+            Console.Write("Please press any button to continue ");
+            Console.ReadKey();
+        }
+
+        public static void Login(string inputNumber, string inputPassword)
+        {
+            var matchingStudent = studentList.Find(student => student.number == inputNumber && student.password == inputPassword);
+
+            if (matchingStudent != null)
+            {
+                Console.WriteLine("Login successful.");
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid number or password.");
+            }
+        }
+    }
+}
---
+++ StudentPanel/Program.cs
@@ -0,0 +1,61 @@
+﻿using StudentPanel;
+
+class Program
+{
+    static void Main()
+    {
+    place:
+        bool isProcess = true;
+        while (isProcess)
+        {
+            Console.WriteLine("Welcome to student panel");
+            Console.WriteLine("1.   Register");
+            Console.WriteLine("2.   List of students");
+            Console.WriteLine("3.   Login");
+            Console.WriteLine("4.   Clear");
+            Console.WriteLine("5.   Exit");
+            Console.Write("Please enter your choice: ");
+            byte cmd = byte.Parse(Console.ReadLine());
+            switch (cmd)
+            {
+                case 1:
+                    Console.Write("Please enter name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Please enter surname: ");
+                    string surname = Console.ReadLine();
+                    Console.Write("Please enter age: ");
+                    byte age = byte.Parse(Console.ReadLine());
+                    Console.Write("Please enter phone number: ");
+                    string number = Console.ReadLine();
+                    Console.Write("Please enter password: ");
+                    string password = Console.ReadLine();
+
+                    recordStudent.AddStudent(name, surname, age, number, password);
+                    break;
+
+                case 2:
+                    recordStudent.DisplayStudents();
+                    break;
+
+                case 3:
+                    Console.Write("Enter number: ");
+                    string number_lg = Console.ReadLine();
+                    Console.Write("Enter password: ");
+                    string password_lg = Console.ReadLine();
+                    recordStudent.Login(number_lg, password_lg);
+                    break;
+                case 4:
+                    Console.Clear();
+                    goto place;
+                case 5:
+                    isProcess = false;
+                    Console.WriteLine("Exiting the program. Goodbye!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a valid option.");
+                    break;
+            }
+        }
+    }
+}
